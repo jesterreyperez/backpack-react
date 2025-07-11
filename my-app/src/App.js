3029d591395d6288b9a9bc1ca57d53f9dcf6825a
Jester Rey Perez <165446384+jesterreyperez@users.noreply.github.com>
2025-07-11 21:53:43 +0800

    Review Calendar documentation for usage, and add feature into App.js file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,48 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Flight Schedule</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { useState } from "react";
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Flight Schedule</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Flight Schedule</h1>
+
+        {/* Calendar component */}
+        <Calendar onChange={setDate} value={date} />
+
+        {/* Display selected date */}
+        <p>Selected date: {date.toDateString()}</p>
+
+        {/* Updated button */}
+        <button>Continue</button>
       </header>
     </div>
   );
